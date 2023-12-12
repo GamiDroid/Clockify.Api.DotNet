9616d4fbadeff27ad6a5ced93791dc01b2af1db7
Kevin Damen <k.damen@bellt.nl>
2023-12-12 21:36:49 +0100

    Add project files.

---
+++ Clockify.Api.DotNet/ClockifyApiClient.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Clockify.Api.DotNet;
+
+public class ClockifyApiClient : IDisposable
+{
+    private readonly HttpClient _httpClient;
+
+    public ClockifyApiClient(string apiKey)
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public void Dispose()
+    {
+        _httpClient.Dispose();
+    }
+}
