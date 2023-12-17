14c7afee901b6ec7f289f4a002d196873222add8
Kevin Damen <k.damen@bellt.nl>
2023-12-17 10:53:34 +0100

    Add custom json datetimeoffset converter

---
+++ Clockify.Api.DotNet/Converters/DateTimeOffsetConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Clockify.Api.DotNet.Converters;
+internal class DateTimeOffsetConverter : JsonConverter<DateTimeOffset>
+{
+    public override DateTimeOffset Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options) =>
+            DateTimeOffset.ParseExact(reader.GetString()!,
+                "yyyy-MM-ddThh:mm:ss.fffZ", CultureInfo.InvariantCulture);
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        DateTimeOffset dateTimeValue,
+        JsonSerializerOptions options)
+    {
+        var dateTimeString = dateTimeValue.ToString("yyyy-MM-ddThh:mm:ss.fffZ", CultureInfo.InvariantCulture);
+        writer.WriteStringValue(dateTimeString);
+    }
+}
